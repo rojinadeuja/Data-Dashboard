267d1ac6dd81a0c0747bfec868d9ca0100ab7c3f
rojinadeuja <rojinadeuja33g@gmail.com>
2020-04-24 18:21:20 -0500

    Added components to App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,37 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; //Import to use bootstrap's CSS classes
 import {BrowserRouter as Router, Route, Link} from "react-router-dom"; //Import to add routing configuration to App.js
 
-import Dashboard from "./components/business.component"; //Import the dashboard component
+import logo from "./yelp_logo.png"; //Import yelp logo
+
+import Dashboard from "./components/dashboard.component"; //Import the dashboard component
+import Business from './components/business.component'; //Import the business component
+
 
 class App extends React.Component{
   render(){
     return(
       <Router>
         <div className="container">
-          <p>YELP TOPIC MODELLING</p>
+          <a class="navbar-brand" href="https://www.yelp.com/" target="_blank">
+            <img src={logo} width="400" height="200" alt="Yelp.com"/>
+          </a>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          {/* <Link to="/" className="nav-link">Yelp Data Dashboard</Link> */}
+            <div className="collpase navbar-collapse">
+            <ul className= "navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">Yelp Data Dashboard</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/business" className="nav-link">Businesses</Link>
+              </li>
+            </ul>
+            </div>
+          </nav>
+          <br/>
+          {/* Add Routes Here */}
+          <Route path="/" exact component={Dashboard}/>
+          <Route path="/businesses" exact component={Business}/>
         </div>
       </Router>
     )
--- src/components/dashboard.component.js
+++ src/components/dashboard.component.js
@@ -4,7 +4,7 @@ export default class Dashboard extends Component{
     render(){
         return(
             <div>
-                <p>YELP DASHBOARD</p>
+                <p>THIS IS THE YELP DASHBOARD</p>
             </div>
         )
     }
