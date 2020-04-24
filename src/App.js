2f9b163a5b75e198c61df1e8d5114fb7284fa5a3
rojinadeuja <rojinadeuja33g@gmail.com>
2020-04-24 18:03:08 -0500

    Added new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; //Import to use bootstrap's CSS classes
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"; //Import to add routing configuration to App.js
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import Dashboard from "./components/business.component"; //Import the dashboard component
+
+class App extends React.Component{
+  render(){
+    return(
+      <Router>
+        <div className="container">
+          <p>YELP TOPIC MODELLING</p>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/business.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Business extends Component{
+    render(){
+        return(
+            <div>
+                <p>BUSINESSES ON YELP</p>
+            </div>
+        )
+    }
+}
